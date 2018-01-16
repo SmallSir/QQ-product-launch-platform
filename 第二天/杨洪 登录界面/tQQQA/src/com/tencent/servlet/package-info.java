89c785f360b43d631d85e8f03f1f8442e8061ddf
SmallSir <280690956@qq.com>
2018-01-16 23:29:04 +0800

    增添第二天任务的登陆界面设计

---
+++ 第二天/杨洪 登录界面/tQQQA/src/com/tencent/servlet/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 主要用于各种控制器的操作
+ */
+/**
+ * @author 杨洪
+ *2018.1.16
+ */
+package com.tencent.servlet;
---
+++ 第二天/杨满香 登录界面/杨满香 登录界面/src/com/tencent/util/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 各种工具类的操作
+ */
+/**
+ * @author 杨洪
+ *2018.1.16
+ */
+package com.tencent.util;
